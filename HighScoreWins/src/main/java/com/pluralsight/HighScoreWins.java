3065f3d9cbbf2e21c1d7106ea149bbc63027b7dd
tashi thompson <tosh@tashis-MacBook-Pro.local>
2024-10-02 16:46:14 -0400

    high score solution

---
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,55 @@
+//Create a new Java application called HighScoreWins that prompts the user to
+//enter team and score information of an event. The input will be a single string
+//that contains both team names and the final score of the match.
+//Prompt the user for a game score.
+//The user will input a score in the following format:    Home:Visitor|21:9
+//Based on the user input, you should determine which team had the higher score
+//and display the name of the winning team.
+
+
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class HighScoreWins {
+
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+
+        //prompt user for information
+        String gameInformation = Prompt("Please enter a game score ");
+
+        //Declare an array to separate the two parts of data (Team and Score)
+        String[] data = gameInformation.split("\\|");  // .split(separate string on "|")
+        String teamNamePart = data[0];  //
+        String teamScorePart = data[1];
+
+        //declare an array to hold  names and score
+        String[] teamName = teamNamePart.split(":");
+        String[] teamScore = teamScorePart.split(":"); // .split(separate string on ":")
+
+        //assigning variables by reference to elements in the array
+        String homeTeam = teamName[0];
+        String awayTeam = teamName[1];
+
+        //Parse(convert) score(String input) into integers
+        int homeScore = Integer.parseInt(teamScore[0]);
+        int awayScore = Integer.parseInt(teamScore[1]);
+
+        if (homeScore > awayScore) {
+            System.out.println("WINNER! " + homeTeam);
+        }
+        else if(homeScore < awayScore) {
+            System.out.println("WINNER! " + awayTeam);
+        }
+        else {
+            System.out.println("TIE !");
+        }
+    }
+
+    public static String Prompt(String prompt){
+        System.out.println(prompt + "(ex. Nets:Bulls | 95:102 )");
+        String userInput = input.nextLine().trim();
+        return userInput;
+    }
+}
---
+++ TheaterReservations/src/main/java/com/pluralsight/TheaterReservation.java
@@ -0,0 +1,15 @@
+//Create a Java application called TheaterReservations. This application is used to
+//allow customers to reserve tickets for a specific date.
+//Prompt the user for their full name, the date of the show, and the number of
+//tickets they will need. Once the reservation is made, display a confirmation message to the customer in
+//this format: # ticket(s) reserved for (date) under (LastName, FirstName)
+// If only one ticket is reserved, the confirmation should not include an "s"
+
+
+package com.pluralsight;
+
+public class TheaterReservation {
+
+
+
+}
